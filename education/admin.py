b6f53c0115ed064baf9b60ad3a919faaa21e7c07
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2026-06-23 00:44:57 +0700

    add model payment

--- education/admin.py
+++ education/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Course, Lesson
+from .models import Course, Lesson, Payment
 
 
 @admin.register(Course)
@@ -12,9 +12,18 @@ class CourseAdmin(admin.ModelAdmin):
 
 
 @admin.register(Lesson)
-class ProductAdmin(admin.ModelAdmin):
+class LessonAdmin(admin.ModelAdmin):
     """Класс представления модели урока в админ-панели Django"""
 
     list_display = ("id", "name", "description", "preview", "link_to_video", "course")
     list_filter = ("course",)
     search_fields = ("name", "description")
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    """Класс представления модели платежа в админ-панели Django"""
+
+    list_display = ("id", "payer", "created_at", "paid_course", "paid_lesson", "amount", "method")
+    list_filter = ("payer", "paid_course", "paid_lesson", "method")
+    search_fields = ("payer", "paid_course", "paid_lesson")
