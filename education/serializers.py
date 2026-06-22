c5b84062450a8b8749e2f95eaf971a77b3fe4d8b
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2026-06-22 14:30:19 +0700

    add field lessons_count to CourseSerializer

--- education/serializers.py
+++ education/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from .models import Course, Lesson
 
@@ -6,11 +6,18 @@ from .models import Course, Lesson
 class CourseSerializer(ModelSerializer):
     """Сериализатор модели курса"""
 
+    lessons_count = SerializerMethodField()
+
     class Meta:
         """Параметры сериализатора"""
 
         model = Course
         fields = "__all__"
+
+    def get_lessons_count(self, course: Course) -> int:
+        """Получение количества уроков в текущем курсе"""
+
+        return course.lessons.count()  # type: ignore
 
 
 class LessonSerializer(ModelSerializer):
